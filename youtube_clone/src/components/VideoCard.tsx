b76a9d58ee926528a1614296a908dd01d7c09250
Sarmanpreet singh <131851235+sarman03@users.noreply.github.com>
2024-06-22 09:59:02 +0000

    changes

---
+++ youtube_clone/src/components/VideoCard.tsx
@@ -0,0 +1,26 @@
+  export function VideoCard(){
+    return <div>
+       <img src="/photo1.jpg" ></img>
+        <div className="grid grid-cols-12">
+
+          <div className="col-span-1">
+          <img className="rounded-full w-20 h-20" src="/photo2.jpg" ></img>
+          </div>
+
+          <div className="col-span-11 pl-5">
+            Peaky Blinder (English)
+          
+
+          <div className="col-span-11 text-gray-600 text-base">
+          Sarman
+          </div>
+
+          <div className="col-span-11  text-gray-600 text-base">
+          46 M | 13 days ago
+          </div>
+          </div>
+        </div>
+        
+
+    </div>
+ }
