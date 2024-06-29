032feb60f2009e5c07b2acc8e3dd667aa69449bc
Sarmanpreet singh <131851235+sarman03@users.noreply.github.com>
2024-06-29 18:36:57 +0000

    database

---
+++ blog site/src/appwrite/auth.js
@@ -0,0 +1,17 @@
+import conf from '../conf.js'
+import {Client, Account, ID} from 'appwrite'
+
+export class AuthService{
+client= new Client();
+account;
+
+constructor(){
+    this.client
+    .set
+}
+
+}
+
+const authService=new AuthService()
+
+export default authService
